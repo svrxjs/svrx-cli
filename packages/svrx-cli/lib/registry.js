992b0d6c143fcb7bd269f2226f23e9b3d4786092
C.Y.Xu <itsxcy@gmail.com>
2019-11-25 15:53:10 +0800

    refactor: update util to 1.1.0, use package-manager for installation

--- packages/svrx-cli/lib/registry.js
+++ packages/svrx-cli/lib/registry.js
@@ -1,11 +1,6 @@
-const path = require('path');
+const { PackageManagerCreator } = require('@svrx/util');
 const { npm, logger } = require('@svrx/util');
 const _ = require('lodash');
-const tmp = require('tmp');
-const fs = require('fs-extra');
-const { fork } = require('child_process');
-const config = require('./config');
-const local = require('./local');
 
 const getVersions = async () => {
   const spinner = logger.progress('Searching versions...');
@@ -45,73 +40,24 @@ const getTags = async () => {
  * @returns {Promise<*>}
  */
 const install = async (version, options = {}) => {
-  const { silent, autoClean, current } = options;
+  const { silent, path } = options;
   const spinner = silent ? null : logger.progress('Installing svrx core package...');
-  const task = fork(path.join(__dirname, './task.js'), {
-    silent: true,
-  });
 
   try {
-    const installedVersion = await new Promise((resolve, reject) => {
-      task.on('error', reject);
-      task.on('message', (ret) => {
-        if (ret.error) reject(new Error(ret.error));
-        else resolve(ret);
-      });
-      task.send({
-        version, versionsRoot: config.VERSIONS_ROOT, autoClean, current,
-      });
+    const pm = PackageManagerCreator({
+      path,
+      version,
     });
+    const pkg = await pm.load();
     if (spinner) spinner();
-    return installedVersion;
+    return pkg.version;
   } catch (e) {
     if (spinner) spinner();
     throw e;
   }
 };
 
-const getInstallTask = async ({
-  version, versionsRoot, autoClean = false, current,
-}) => {
-  const versions = await getVersions();
-  versions.reverse();
-
-  const installVersion = version || versions[0];
-
-  if (local.exists(installVersion, versionsRoot)) return installVersion; // already installed
-
-  const tmpObj = tmp.dirSync();
-  const tmpPath = tmpObj.name;
-  const options = {
-    name: '@svrx/svrx',
-    version: installVersion,
-    global: true,
-    path: tmpPath,
-  };
-
-  const result = await npm.install(options);
-  const svrxRoot = path.resolve(tmpPath, 'node_modules/@svrx/svrx');
-  const destFolder = path.resolve(versionsRoot, result.version);
-  const destFolderDependency = path.resolve(versionsRoot, result.version, 'node_modules');
-
-  return new Promise((resolve) => {
-    fs.copySync(svrxRoot, destFolder);
-    fs.copySync(path.resolve(tmpPath, 'node_modules'), destFolderDependency);
-
-    if (autoClean) {
-      fs.writeFileSync(path.resolve(destFolder, '.autoclean'), '');
-      // auto clean old packages with .autoclean label
-      local.cleanOlds(installVersion, current, versionsRoot).then(() => {
-        resolve(installVersion);
-      });
-    } else {
-      resolve(installVersion);
-    }
-  });
-};
-
 module.exports = {
-  getInstallTask,
   install,
   getVersions,
   getTags,
